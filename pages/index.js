a16987770e489689629cda1149626feb19bcfb39
baekjiyeon <whitedelay@gmail.com>
2021-05-10 00:48:49 +0900

    Implement home page
    - Add image
    - Add list of posts

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,32 @@
-import Link from "next/link";
-
-import { Layout, Bio, SEO } from "@components/common";
+import { Layout } from "@components/Layout";
+import { PostCard } from "@components/PostCard";
+import { SEO } from "@components/Seo";
+import { Image } from "@components/Image";
 import { getSortedPosts } from "@utils/posts";
 
 export default function Home({ posts }) {
   return (
     <Layout>
-      <SEO title="All posts" />
-      <Bio className="my-14" />
-      {posts.map(({ frontmatter: { title, description, date }, slug }) => (
-        <article key={slug}>
-          <header className="mb-2">
-            <h3 className="mb-2">
-              <Link href={"/post/[slug]"} as={`/post/${slug}`}>
-                <a className="text-4xl font-bold text-yellow-600 font-display">
-                  {title}
-                </a>
-              </Link>
-            </h3>
-            <span className="text-sm">{date}</span>
-          </header>
-          <section>
-            <p className="mb-8 text-lg">{description}</p>
-          </section>
+      <SEO title="Home" />
+      <Image
+        src={require(`../content/assets/profile-light.jpg`)}
+        webpSrc={require(`../content/assets/profile-light.jpg?webp`)}
+        previewSrc={require(`../content/assets/profile-light.jpg?lqip`)}
+        className="max-w-screen-lg w-overflow -mx-5 h-60 sm:h-96 rounded mb-2"
+      />
+      <div className="font-bold font-serif text-xl sm:text-2xl p-2 px-4 text-gray-700 dark:text-gray-200">
+        All posts
+      </div>
+      {posts.map((post) => (
+        <article key={post.slug}>
+          <PostCard slug={post.slug} {...post.frontmatter} />
         </article>
       ))}
     </Layout>
   );
 }
 
-export async function getStaticProps() {
+export function getStaticProps() {
   const posts = getSortedPosts();
 
   return {
