9c8ef3c2af9bea5798a31969a20daae8859f8733
baekjiyeon <whitedelay@gmail.com>
2021-05-14 22:21:15 +0900

    Change front image to short bio

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
 import { Layout } from "@components/Layout";
 import { PostCard } from "@components/PostCard";
 import { SEO } from "@components/Seo";
-import { Image } from "@components/Image";
+import { FiGithub, FiMail } from "react-icons/fi";
 import { getSortedPosts } from "@utils/posts";
 
 export default function Home({ posts }) {
   return (
     <Layout>
       <SEO title="Home" />
-      <Image
-        src={require(`../content/assets/profile-light.jpg`)}
-        webpSrc={require(`../content/assets/profile-light.jpg?webp`)}
-        previewSrc={require(`../content/assets/profile-light.jpg?lqip`)}
-        className="max-w-screen-lg w-overflow -mx-5 h-60 sm:h-96 rounded mb-2"
-      />
-      <div className="font-bold font-serif text-xl sm:text-2xl p-2 px-4 text-gray-700 dark:text-gray-200">
-        All posts
-      </div>
+      <section className="border-dashed border border-gray-300 dark:border-gray-700 rounded mx-4 mb-8 p-4 text-sm sm:text-base">
+        기록을 통해 매일 매일 더 나아지고 싶은, 개발자 <strong>백</strong>
+        <sup>white</sup>
+        <strong>지연</strong>
+        <sup>delay</sup>
+        입니다.
+        <br />
+        <br />
+        <span className="flex items-center">
+          <FiGithub className="w-4 h-5 mt-1" />
+          <a className="mx-2" href="https://github.com/whitedelay">
+            github
+          </a>
+        </span>
+        <span className="flex items-center">
+          <FiMail className="w-4 h-5 mt-1" />
+          <a className="mx-2" href="mailto:whitedelay@gmail.com">
+            email
+          </a>
+        </span>
+      </section>
+      <span className="font-body font-bold text-base sm:text-lg p-2 px-4 text-gray-700 dark:text-gray-300">
+        전체 포스트
+      </span>
       {posts.map((post) => (
         <article key={post.slug}>
           <PostCard slug={post.slug} {...post.frontmatter} />
